0b0f659f12aaf5d75707f1854a1e03964b7e7e48
bentwhiskers <abbott1414@gmail.com>
2023-08-15 23:50:11 -0400

    fixed path in routes/index.js for /api

--- utils/seed.js
+++ utils/seed.js
@@ -2,12 +2,32 @@ const connection = require('../config/connection');
 const { User, Thought } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const thoughtSeeds = require('./thoughtSeeds.json');
+const clearDB = require('./clearDB');
 
 connection.once('open', async () => {
+    await clearDB('Thought', 'thoughts', 'User', 'users');
+    await User.deleteMany({});
     await User.create(userSeeds);
     await Thought.create(thoughtSeeds);
 
     console.log('all done!');
     process.exit(0);
 });
+
+
+User.insertMany(userSeeds)
+    .then(docs => {
+        console.log(docs);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+Thought.insertMany(thoughtSeeds)
+    .then(docs => {
+        console.log(docs);
+    })
+    .catch(error => {
+        console.error(error);
+    });
 
